c3f54d945165ea25166d1339429a7e7e1928598e
Mônica Ribeiro <monicarib1@gmail.com>
2019-12-11 11:20:37 -0300

    First commit.

---
+++ src/main/java/com/example/movieunitandintegrationtests/MovieUnitAndIntegrationTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieunitandintegrationtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieUnitAndIntegrationTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieUnitAndIntegrationTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movieunitandintegrationtests/MovieUnitAndIntegrationTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieunitandintegrationtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieUnitAndIntegrationTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
