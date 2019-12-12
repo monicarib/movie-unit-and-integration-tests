08e186cd85d0e2577e83076d4aeadd64eee800c6
Mônica Ribeiro <monicarib1@gmail.com>
2019-12-12 19:00:56 -0300

    Melhorando teste.

--- src/test/java/com/example/movieunitandintegrationtests/api/MovieControllerTest.java
+++ src/test/java/com/example/movieunitandintegrationtests/api/MovieControllerTest.java
@@ -9,9 +9,12 @@ import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -31,7 +34,7 @@ public class MovieControllerTest extends BaseIntegrationTest {
                 .queryParam("title", title))
                 .andExpect(status().isOk())
                 .andExpect(content()
-                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-//                .andExpect((ResultMatcher) jsonPath("isRecommended", equals("true")));
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.recommended").value("true"));
     }
 }
