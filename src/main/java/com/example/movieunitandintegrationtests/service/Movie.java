50e88df7aca1c672e6276c9ed2d63db70a56ae0a
Mônica Ribeiro <monicarib1@gmail.com>
2019-12-12 14:31:46 -0300

    Implementando domínio do service.

---
+++ src/main/java/com/example/movieunitandintegrationtests/service/Movie.java
@@ -0,0 +1,76 @@
+package com.example.movieunitandintegrationtests.service;
+
+public class Movie {
+    public String title;
+    public String released;
+    public String runtime;
+    public String genre;
+    public String director;
+    public String plot;
+    public String imdbRating;
+    public Boolean isRecommended;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(String imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public Boolean getRecommended() {
+        return isRecommended;
+    }
+
+    public void setRecommended(Boolean recommended) {
+        isRecommended = recommended;
+    }
+}
